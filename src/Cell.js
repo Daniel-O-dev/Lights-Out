16ae1c230dc97fb64c7ef1df5b7bc6b6af6693ee
Daniel <danieloyewole11@gmail.com>
2022-06-13 14:02:23 +0100

    first commit

---
+++ src/Cell.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import "./Cell.css"
+
+
+
+class Cell extends Component {
+ 
+  handleClick = (evt) => {
+    // call up to the board to flip cells around this cell
+    this.props.flipCellsAroundMe();
+  }
+
+  render() {
+    let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
+
+    return (
+        <td className={classes} onClick={this.handleClick} />
+    )
+  }
+}
+
+
+export default Cell
